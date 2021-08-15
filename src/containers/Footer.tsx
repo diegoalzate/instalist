512a02d11b1067cf17e991309e1fefbbfb19259d
Carlosayol <Carlosaolar@gmail.com>
2021-08-14 19:29:18 -0500

    feat: Containers initial setup
    basic structure for header and footer with a layout

---
+++ src/containers/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer>
+            <p>Copyright ⓒ {currentYear}</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/containers/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Instalist</h1>
+        </div>
+    );
+}
+
+export default Header;
