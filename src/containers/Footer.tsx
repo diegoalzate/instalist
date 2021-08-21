a6c3af6f271ee26950ead6bc816b1cc6648e6dbe
Carlosayol <Carlosaolar@gmail.com>
2021-08-21 11:37:40 -0500

    feat: remove unnecessary code

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -1,5 +1,4 @@
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
     return (
         <footer className="fixed border-gray-200 border-t-2 inset-x-0 text-sm justify-between bottom-0 text-center bg-red-400 text-gray-100 font-semibold px-6 mx-auto flex">
             <a href="#" className="w-full block p-3 text-center">Profile</a>
