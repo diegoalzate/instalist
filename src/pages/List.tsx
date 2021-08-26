2db8777cf85b030c153814ad9cc4ba942423b2d2
Diego <diego35.da@gmail.com>
2021-08-25 21:59:28 -0500

    feat: input for list and button with basic logic

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react"
 const List = () => {
+    const [wish, setWish] = useState("")
+    const [wishes, setWishes] = useState<String[]>([])
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {value} = e.target
+        setWish(value)
+    }
+    const addItem = (e: React.MouseEvent<HTMLButtonElement>) => {
+        setWishes([...wishes, wish])
+    }
     return (
-        <div className="container">
-            <h1>hi</h1>
+        <div className="container flex flex-col justify-center">
+            <h1 className="self-center mb-4">my list</h1>
+            <div className="self-center space-x-3">
+                <input 
+                    className="shadow-md text-sm text-gray-700 rounded p-2 focus:outline-none" 
+                    name="wish" 
+                    onChange={handleChange} 
+                    value={wish}
+                    placeholder="fidget spinner..."
+                />
+                <button className="rounded bg-red-400 py-1 px-4 text-gray-100" onClick={addItem}>add</button>
+            </div>
+            <section id="wishes">
+                <ul>
+                    {wishes.map(wish => (
+                        <li>{wish}</li>
+                    ))}
+                </ul>
+            </section>
         </div>
     )
 }
