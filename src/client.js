6318cd73c0bcc22cca445c3ff8cb1ce45963e226
Diego Alzate <diegoalzate@Diegos-MacBook-Air.local>
2022-02-07 19:07:16 -0500

    chore: hide env variables

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-    "https://bpxvygqgphuornaexayl.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTY1MDkyNiwiZXhwIjoxOTQ1MjI2OTI2fQ.h87JOH4Mnb9hdcBNAXXjJVUyuy1BsIqOqjwtRPJbqk4"
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_ANON_KEY
 )
