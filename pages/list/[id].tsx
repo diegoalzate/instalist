c8317583747437de658a20c3718bef9c363ffd7d
Diego Alzate <diego35.da@gmail.com>
2022-07-24 23:10:04 -0500

    feat: private lists and ui fixes

--- pages/list/[id].tsx
+++ pages/list/[id].tsx
@@ -19,7 +19,7 @@ const Guest = () => {
   const [selectedList, setSelectedList] = useState<ListType | undefined>()
   
   useEffect(() => {
-    if (data && !selectedList) {
+    if (data && !selectedList && !data?.[0].locked) {
       setSelectedList(data?.[0])
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -27,7 +27,7 @@ const Guest = () => {
 
   useEffect(() => {
     if(selectedList) {
-      if (selectedList.profile_id !== profile?.id) {
+      if (selectedList.profile_id !== profile?.id && !selectedList.locked) {
         addViewIfNotOwner()
       }
     }
